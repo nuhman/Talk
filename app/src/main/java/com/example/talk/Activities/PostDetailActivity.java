4e80a94bb55642d77922cbb8c6802a8e49093e09
Muhammed Nuhman <nuhman7@outlook.com>
2019-04-25 22:14:25 +0530

    Post Details layout design

---
+++ app/src/main/java/com/example/talk/Activities/PostDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.talk.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.talk.R;
+
+public class PostDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_detail);
+    }
+}
